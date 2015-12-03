b601316dda9c21b156c1726e3a6559fa949c12c6
Vladimir S. Kuznetsov <vladimirkz@confirmit.com>
2015-12-03 15:09:59 +0300

    Packages update

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,16 +5,16 @@ sudo apt-get -qqy install default-jdk
 cd /vagrant
 mkdir doc
 cd doc/
-wget http://russianaicup.ru/s/1447999864318/assets/documentation/coderacing2015-docs.pdf
+wget http://russianaicup.ru/s/1449064144808/assets/documentation/coderacing2015-docs.pdf
 cd ../
 mkdir Runner
 cd Runner/
-wget http://russianaicup.ru/s/1448458279522/assets/local-runner/local-runner.zip
+wget http://russianaicup.ru/s/1449064144808/assets/local-runner/local-runner.zip
 unzip local-runner.zip
 sudo rm -f local-runner.zip
 cd ../
 mkdir Repeater
 cd Repeater/
-wget http://russianaicup.ru/s/1448458279522/assets/repeater/repeater.zip
+wget http://russianaicup.ru/s/1449064144808/assets/repeater/repeater.zip
 unzip repeater.zip
 sudo rm -f repeater.zip
